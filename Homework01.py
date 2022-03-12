5f198b25579970542884de5b5fec7894fdc58a05
zhu-2002 <1981941392>
2022-03-12 23:36:45 +0800

    message

---
+++ Homework01.py
@@ -0,0 +1,73 @@
+import operator as op
+
+#2
+def k_in_num(k, num):
+    """
+    Complete k_in_num, a function which returns True if num has the digit k and
+    returns False if num does not have the digit k. 0 is considered to have no
+    digits.
+    >>> k_in_num(3, 123) # .Case 1
+    True
+    >>> k_in_num(2, 123) # .Case 2
+    True
+    >>> k_in_num(5, 123) # .Case 3
+    False
+    >>> k_in_num(0, 0) # .Case 4
+    False
+    """
+    while num:
+        if k == num % 10:
+            return True
+        num = num // 10
+    return False
+#3
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    >>> a_plus_abs_b(-1, 4)
+    3
+    >>> a_plus_abs_b(-1, -4)
+    3
+    """
+    if b < 0:
+        f = op.sub
+    else:
+        f = op.add
+    return f(a, b)
+#4
+def two_of_three(i, j, k):
+    """Return m*m + n*n, where m and n are the two smallest members of the
+    positive numbers i, j, and k.
+
+    >>> two_of_three(1, 2, 3)
+    5
+    >>> two_of_three(5, 3, 1)
+    10
+    >>> two_of_three(10, 2, 8)
+    68
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    return pow(i,2)+pow(j,2)+pow(k,2)-pow(max(i,j,k),2)
+#5
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    m = n - 1
+    while m > 0 :
+        if n%m == 0 :
+            return m ;
+        m -= 1
+
