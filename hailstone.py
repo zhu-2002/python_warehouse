2ff48d8a3986cb08565face25d198edc12fc8a9d
zhu-2002 <1981941392>
2022-03-20 20:36:39 +0800

    hailstone.py

--- hailstone.py
+++ hailstone.py
@@ -18,3 +18,15 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    k = 1
+    print(n)
+    while n != 1:
+        k = k + 1
+        if n % 2 == 0:
+            n = n // 2
+            print(n)
+        else:
+            n = n * 3 + 1
+            print(n)
+    return k
+
