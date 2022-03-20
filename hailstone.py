e6d113a3d0136b353cf932b073314ae76e87855f
zhu-2002 <1981941392>
2022-03-20 20:35:07 +0800

    hailstone.py

---
+++ hailstone.py
@@ -0,0 +1,20 @@
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    "*** YOUR CODE HERE ***"
